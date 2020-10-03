9eff403fefc7e6f83067afc86170fe60728b8130
root <root@localhost.localdomain>
2020-10-03 17:00:38 -0300

    Updates

---
+++ precache-manifest.22a8d10150aa3b8f239c0945eb2ff968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6d15ac47cfb0e8f1633bf026c7cdd9",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "704de3d4c262711c3db8",
+    "url": "/todoapp/static/css/main.2eedca42.chunk.css"
+  },
+  {
+    "revision": "0d8be065246f0aa0f748",
+    "url": "/todoapp/static/js/2.30b5c093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.30b5c093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "704de3d4c262711c3db8",
+    "url": "/todoapp/static/js/main.d0c53db7.chunk.js"
+  },
+  {
+    "revision": "96dc3aa3f30eeded0550",
+    "url": "/todoapp/static/js/runtime-main.5c0369ef.js"
+  }
+]);
